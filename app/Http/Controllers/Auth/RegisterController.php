1b5bef6092dd137367e2e12091b2eeffcf498a24
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-09-04 14:02:18 +0700

    laravel auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,6 +48,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
+            'nopeg' => 'required',
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
@@ -63,9 +64,11 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'nopeg' => $data['nopeg'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'admin_to_live' => $data['admin_to_live'],
         ]);
     }
 }
--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nopeg', 'name', 'email', 'password', 'admin_to_live',
     ];
 
     /**
