ec70be25c8c7b967963761a98648ef1ef249acb3
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-09-06 14:07:38 +0700

    create database message table

---
+++ database/migrations/2023_09_04_154233_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('from_user');
+            $table->string('to_user');
+            $table->text('content');
+            $table->timestamps();
+
+            $table->foreign('from_user')->references('nopeg')->on('users');
+            $table->foreign('to_user')->references('nopeg')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
