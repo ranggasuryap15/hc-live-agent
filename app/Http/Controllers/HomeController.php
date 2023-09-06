1afa6fa13ec081cb5e321eb00e031a74ff5e6c07
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-09-06 14:06:16 +0700

    passing data user login ke views

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // $users = User::where('id', '=', Auth::user()->id)->get();
+        $user = DB::table('users')->select('*')->where('id', '=', Auth::user()->id)->first();
+        return view('home', ['user' => $user]);
     }
 }
