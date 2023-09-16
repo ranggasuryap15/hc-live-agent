6dfcbc1d5670e7610cbd30d8695e3c2db0ef2891
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-09-16 20:01:35 +0700

    memperbaiki halaman chat client

--- resources/views/chat-hc.blade.php
+++ resources/views/chat-hc.blade.php
@@ -10,26 +10,24 @@
 
 <body>
     <div class="container chat_box" id="chat_box">
-        {{-- <div id="chat_box" class="chat_box"> --}}
-            <div class="header">
-                <input action="action" onclick="window.history.back(); return false;" type="submit" value="Back"
-                    class="back-navigation" />
-                <h1>HC Live Agent</h1>
-            </div>
-            <div class="body chat-area">
-                {{-- disini akan muncul chat berdasarkan pengguna login --}}
-            </div>
-            <div class="send-area">
-                <input type="text" class="chat_input" id="chat_input" name="" placeholder="Type a message" autofocus>
-                <button type="button" class="btn-chat" data-to-user="{{ \Auth::user()->admin_to_live }}"
-                    disabled>Send</button>
-            </div>
-            <input type="hidden" id="to_user_nopeg" value="{{ \Auth::user()->admin_to_live }}" />
-            <input type="hidden" id="current_user" value="{{ \Auth::user()->nopeg }}" />
-            <input type="hidden" id="pusher_app_key" value="{{ env('PUSHER_APP_KEY') }}" />
-            <input type="hidden" id="pusher_cluster" value="{{ env('PUSHER_APP_CLUSTER') }}" />
+        <div class="header">
+            <input action="action" onclick="window.history.back(); return false;" type="submit" value="Back"
+                class="back-navigation" />
+            <h1>HC Live Agent</h1>
         </div>
-        {{-- </div> --}}
+        <div class="body chat-area">
+            {{-- disini akan muncul chat berdasarkan pengguna login --}}
+        </div>
+        <div class="send-area">
+            <input type="text" class="chat_input" id="chat_input" name="" placeholder="Type a message" autofocus>
+            <button type="button" class="btn-chat" data-to-user="{{ \Auth::user()->admin_to_live }}"
+                disabled>Send</button>
+        </div>
+        <input type="hidden" id="to_user_nopeg" value="{{ \Auth::user()->admin_to_live }}" />
+        <input type="hidden" id="current_user" value="{{ \Auth::user()->nopeg }}" />
+        <input type="hidden" id="pusher_app_key" value="{{ env('PUSHER_APP_KEY') }}" />
+        <input type="hidden" id="pusher_cluster" value="{{ env('PUSHER_APP_CLUSTER') }}" />
+    </div>
 
     <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="https://js.pusher.com/4.1/pusher.min.js"></script>
