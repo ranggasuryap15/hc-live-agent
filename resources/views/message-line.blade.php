82d0cdc9f96dae8a6dcb7d5116db1efa7639ca56
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-09-20 09:09:33 +0700

    message line terbaru

--- resources/views/message-line.blade.php
+++ resources/views/message-line.blade.php
@@ -10,7 +10,7 @@
     <div class="flex items-end">
         <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
             <div>
-                <p data-message-id=""
+                <p data-message-id="{{ $message->id }}"
                     class="px-2 py-2 rounded-lg inline-block rounded-br-none bg-green-200 bg-200 text-black-600">
                     {{ $message->content }}
                 </p>
@@ -36,7 +36,7 @@
     <div class="flex items-end">
         <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
             <div>
-                <p data-message-id=""
+                <p data-message-id="{{ $message->id }}"
                     class="px-2 py-2 rounded-lg inline-block rounded-bl-none bg-gray-200 bg-200 text-gray-600">
                     {{ $message->content }}
                 </p>
